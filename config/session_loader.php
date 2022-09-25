808548b546ed1a1dee495e9c0ee1360e59f52137
Aristote ENGUDI <engudiaristot@gmail.com>
2022-09-25 19:43:42 +0200

    active twig session

--- config/session_loader.php
+++ config/session_loader.php
@@ -1,13 +1,12 @@
 <?php
-/*
+
 session_start();
 $unique_id = isset($_SESSION['unique_id']) ? $_SESSION['unique_id'] : false;
 
 global $db;
-$query = $db->fetchAssoc('SELECT * FROM tb_users WHERE unique_id = ?',array($unique_id));
+$query = $db->fetchAssociative('SELECT * FROM t_users WHERE uid = ?',array($unique_id));
 if(empty($query)) {
     require_once "./src/Pages/login.php";
     exit;
 }
 
-*/
