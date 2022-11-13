050909684d7cee419b08180af37bab43497cb93e
Aristote ENGUDI <engudiaristot@gmail.com>
2022-11-13 18:48:36 +0100

    create reservations

---
+++ src/Model/Reservations.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Model;
+
+
+use App\Classes\db;
+use App\Classes\Security;
+
+
+class Reservations extends db
+{
+    private $reservation_id;
+    public $stardate;
+    public $endate;
+    public $times;
+    public $reservation_uid;
+    public $chambre_uniqid;
+    public $prix;
+    public $status_reservation;
+    public $client_uniqid;
+    public $nombre_personne;
+
+
+
+    public function createReservation(){
+
+        $this->db->beginTransaction();
+
+        try{
+
+            $this->db->insert('t_reservations',
+                array(
+                    'stardate'=> $this->stardate,
+                    'endate'=> $this->endate,
+                    //'times' => '',
+                    'reservation_uid'=> $this->getReservationUid(),
+                    'chambre_uniqid'=> $this->chambre_uniqid,
+                    'prix'=> $this->prix,
+                    'status_reservation'=> 'actif',
+                    'client_uniqid'=> $this->client_uniqid,
+                    'nombre_personne'=> $this->nombre_personne,
+                    'created_at'=>$this->getCreatedDate(),
+                    'updated_at'=>$this->getUpdatedDate()
+                )); // save reservations
+
+
+
+            $this->db->update('t_chambres',
+                array('disponibilite'=>'indisponible'),
+                array('numero_chambre'=>$this->chambre_uniqid)); // update chambre state
+
+            $this->db->commit();
+
+        }catch (\Exception $exception){
+            $this->db->rollBack();
+            throw $exception;
+
+        }
+    }
+
+
+    private function getStarDate(){}
+    private function getEndDate(){}
+
+    private function getReservationUid(){ return Security::randomizer_integer(10,979);}
+    private function getCreatedDate(){ return date('Y-m-d H:i:s');}
+
+    private function getUpdatedDate(){ return date('Y-m-d H:i:s');}
+}
