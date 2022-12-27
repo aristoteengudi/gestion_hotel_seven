e6cad68d3aaec31839088cbb9450cfdfbc3a9362
Aristote ENGUDI <33156652+aristoteengudi@users.noreply.github.com>
2022-12-27 11:14:55 +0100

    change etat_disponibilite

--- src/Model/Reservations.php
+++ src/Model/Reservations.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 
+use App\Chart\ChartOptions;
 use App\Classes\db;
 use App\Classes\Security;
 
@@ -67,6 +68,16 @@ class Reservations extends db
         $query = $this->db->fetchAssociative('SELECT count(*) as count_total_reservation FROM t_reservations');
 
         return $query;
+    }
+
+    public function getCurrentMonthReservationAmount(){
+
+        $sql  = "SELECT  SUM(`prix`) AS montant_total_reservation_mensuel
+                    FROM `t_reservations` WHERE MONTH(`created_at`) = MONTH(NOW());";
+
+        $query = $this->db->fetchAssociative($sql);
+
+        return $query;
 
     }
 
@@ -80,4 +91,73 @@ class Reservations extends db
     private function getCreatedDate(){ return date('Y-mc-d H:i:s');}
 
     private function getUpdatedDate(){ return date('Y-m-d H:i:s');}
+
+
+    public function getChartHistogramReservMonth(){
+
+        $get = \GuzzleHttp\json_decode(file_get_contents('php://input'),true);
+        $start_date = str_replace('/','-',$get['start_date']);
+        $start_date = date('Y-m-d',strtotime($start_date));
+
+        $end_date = str_replace('/','-',$get['end_date']);
+        $end_date = date('Y-m-d',strtotime($end_date));
+        $array = array($start_date,"$end_date 23:59:59");
+
+        $query = $this->db->fetchAllAssociative("select count(*) as count_, `ocs_result_code`,`ocs_result_desc`,
+                                case `ocs_result_code`
+                                    when '0' then 'Réussi'
+                                    when 'S-ACT-00112' then 'Balance insuffisant'
+                                    when 'S-DAT-00021' then 'Echec mise à jour requête'
+                                    else 'Echec'
+                                end as state
+                                
+                                from `tb_logs_facturation` where `created_at` between ? and ? 
+                                group by state",$array);
+
+        $new_array = array();
+        $series_name = array();
+        $details = '';
+
+        if ($get['start_date']==$get['end_date']){
+            $details = "Rapport du {$get['start_date']}";
+
+        }else{
+            $details = "Rapport du {$get['start_date']} au {$get['end_date']}";
+        }
+
+        $chart = [];
+        $chart['container'] = "facturation_histogram";
+        $cOptions = new ChartOptions();
+        $cOptions->setChart("column",false,null,null);
+        $cOptions->setTitle($details);
+        $cOptions->setShadow(['text'=>false]);
+        $cOptions->setTooltip(['pointFormat' =>'{series.name}: <b>{point.y}</b>','headerFormat'=>'<b>{series.name}</b><br><br>']);
+        $cOptions->setXAxis('','',false);
+        $cOptions->setYAxis('',['text'=>'Valeur']);
+        //$cOptions->setAccessibility(['point'=>['valueSuffix'=> '%']]);
+        $cOptions->setPlotOptions( [
+            'series'=>[
+                'dataLabels'=>[
+                    'enabled'=>true,
+                    'color'=>'#000',
+                    'style'=>['fontWeight'=>'bolder'],
+                    'inside' =>true,
+                ],
+                'pointPadding'=>'0.1',
+                'groupPadding'=>'0',
+            ]
+        ]);
+        foreach ($query as $value){
+            $cOptions->appendSeries((string)$value['state'],[(int)$value['count_']],false);
+
+            //$new_array [] = array('name'=>(string)$value['region_name'],'data'=>[(int)$value['total']]);
+        }
+
+        $options = $cOptions->getOptions();
+
+        $chart = array_merge($chart, $options);
+
+
+        $response  = \GuzzleHttp\json_encode($chart);
+    }
 }
