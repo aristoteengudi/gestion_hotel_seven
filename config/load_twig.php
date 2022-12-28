14bf4e43361904305537e2eaf2d1793a3ba6bd56
Aristote ENGUDI <33156652+aristoteengudi@users.noreply.github.com>
2022-12-28 11:28:22 +0100

    add two three new  function : refreshCurent, refreshSpecific, redirect

--- config/load_twig.php
+++ config/load_twig.php
@@ -9,3 +9,30 @@ function render($tpl, $params){
     global $twig;
     echo $twig->render('pages/'.$tpl, $params);
 }
+
+function redirectUrl($path,$flash_message = ''){
+
+    $path = getBaseUrl().$path;
+
+    $_SESSION['flash_message'] = !empty($flash_message) ? $flash_message : false;
+
+    header('location:' .$path);
+
+    //unset($_SESSION['flash_message']);
+
+}
+
+
+function refreshCurrent(){
+    header("Refresh:0");
+}
+
+function refreshSpecific($path,$flash_message){
+
+    $path = getBaseUrl().$path;
+
+    $_SESSION['flash_message'] = !empty($flash_message) ? $flash_message : false;
+
+    header("Refresh:0; url={$path}");
+    //unset($_SESSION['flash_message']);
+}
