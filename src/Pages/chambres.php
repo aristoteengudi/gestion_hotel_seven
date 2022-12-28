7b11cf0d9aaf6ff19d54efccfdb6cc8e053957b0
Aristote ENGUDI <33156652+aristoteengudi@users.noreply.github.com>
2022-12-28 19:40:09 +0100

    date and filter

--- src/Pages/chambres.php
+++ src/Pages/chambres.php
@@ -10,7 +10,22 @@ $params = ['page_title'=>'Nos Chambres', 'breadcrumb' => $breadcrumb];
 $action = app_request();
 
 switch ($action){
-    case 'insert':
+    case 'filter':
+
+        $chambre = new \App\Model\Chambres();
+
+        $start_date = $_GET['start_date'];
+        $end_date = $_GET['end_date'].' 23:59:59';
+        $room_status = $_GET['room_status'];
+
+        $params ['filter_data'] = $chambre->getChambreWithOtherDetailsByStatus($room_status,$start_date,$end_date);
+
+        $params ['start_date'] = $_GET['start_date'];
+        $params ['end_date'] = $_GET['end_date'];
+        $params ['room_status'] = $_GET['room_status'];
+
+        render('chambres.html.twig', $params);
+
 
         break;
     case 'edit':
