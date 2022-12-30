a02323e613afcc0f4da9e2c7b3c19e5494886699
Aristote ENGUDI <33156652+aristoteengudi@users.noreply.github.com>
2022-12-30 10:48:36 +0100

    update code

--- src/Pages/reservations.php
+++ src/Pages/reservations.php
@@ -30,6 +30,26 @@ $date_time = date('Y-m-d H:i:s');
 
 
 switch ($action){
+
+    case 'filter':
+
+        $chambre = new \App\Model\Chambres();
+
+        $start_date = $_GET['start_date'];
+        $end_date = $_GET['end_date'].' 23:59:59';
+        $room_status = $_GET['room_status'];
+
+        $params ['filter_data'] = $chambre->getChambreWithOtherDetailsByStatus($room_status,$start_date,$end_date);
+
+
+        $params ['start_date'] = $_GET['start_date'];
+        $params ['end_date'] = $_GET['end_date'];
+        $params ['room_status'] = $_GET['room_status'];
+
+
+        render('reservations.html.twig', $params);
+
+        break;
     case 'get_chambre_details':
 
         $chambre_id = \GuzzleHttp\json_decode(file_get_contents('php://input'),true);
